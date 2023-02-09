cfe0c7a4af775ce236e82cce234270559f311b3b
sergiosag <sarranz.sag@gmail.com>
2023-02-09 16:04:11 +0100

    feat: implements components routes & navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokeDetailsComponent } from './poke-details/poke-details.component';
+import { PokeFinderComponent } from './poke-finder/poke-finder.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: PokedexComponent,
+},
+{
+  path: 'pokedex',
+  component: PokedexComponent,
+},
+{
+  path: 'pokefinder',
+  component: PokeFinderComponent,
+},
+{
+  path: 'details/:pokeid',
+  component: PokeDetailsComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokeFinderComponent } from './poke-finder/poke-finder.component';
+import { PokeDetailsComponent } from './poke-details/poke-details.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokedexComponent,
+    PokeFinderComponent,
+    PokeDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
