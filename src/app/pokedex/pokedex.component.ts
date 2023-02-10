8782305d0fff10c7e3e59ec1e07a642bdcb73e05
sergiosag <sarranz.sag@gmail.com>
2023-02-10 10:24:33 +0100

    fix: pokedex undefined error

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { PokeApiService, PokedexInterface, PokemonEntriesInterface } from '../poke-api.service';
 
 @Component({
@@ -6,19 +7,27 @@ import { PokeApiService, PokedexInterface, PokemonEntriesInterface } from '../po
   templateUrl: './pokedex.component.html',
   styleUrls: ['./pokedex.component.scss']
 })
-export class PokedexComponent implements OnInit {
-  pokeList: PokemonEntriesInterface[] = []
+export class PokedexComponent implements OnInit, OnDestroy {
+  pokeList: PokemonEntriesInterface[] = [];
+  subscription: Subscription | undefined;
   constructor ( private pokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.pokeApiService.getPokedexObservable().subscribe((pokedex: PokedexInterface[])=>{
-      this.onPokedex(pokedex[pokedex.length - 1])
+   this.subscription = this.pokeApiService.getPokedexObservable().subscribe((pokedex: PokedexInterface[])=>{
+      this.onPokedex(pokedex[pokedex?.length - 1])
     })  
   }
 
   private onPokedex(pokedex: PokedexInterface){
-    if(!pokedex) this.pokeApiService.getPokedex();
+    if(!pokedex) {
+      this.pokeApiService.getPokedex();
+      return;
+    }
     this.pokeList = pokedex.pokemon_entries
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
 }
